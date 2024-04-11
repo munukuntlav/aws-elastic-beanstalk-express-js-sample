1fcd4a76c571531ec04af88fc69264c45072ef2d
munukuntlav <vmunuk@my.bridgeport.edu>
2024-04-11 18:54:19 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello world Welcome to CLoud Computing'));
+app.get('/', (req, res) => res.send('cpsc 571 Welcome to CLoud Computing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
