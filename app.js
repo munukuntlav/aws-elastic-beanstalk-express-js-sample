2d541bb6523ba998c6513abe154ea5816041bb59
munukuntlav <vmunuk@my.bridgeport.edu>
2024-04-17 22:57:03 -0400

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Cloud Computing'));
+app.get('/', (req, res) => res.send('Hello Welcome Welcome World Cloud Computing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
